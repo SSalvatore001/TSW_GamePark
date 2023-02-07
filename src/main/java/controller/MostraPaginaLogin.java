f318619cbd8435c36e15375e163a77cfe55b083a
Salvatore Sautariello <sautariellos@gmail.com>
2023-02-07 02:29:23 +0100

    fix : aggiustato import e package

--- src/main/java/controller/MostraPaginaLogin.java
+++ src/main/java/controller/MostraPaginaLogin.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 @WebServlet("/MostraPaginaLogin")
 public class MostraPaginaLogin extends HttpServlet {
 
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
