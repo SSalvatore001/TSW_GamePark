1755d3809a77f7f0f220b4103620f46e49aa28bf
Salvatore Sautariello <sautariellos@gmail.com>
2023-02-07 02:26:02 +0100

    fix : aggiustato import e package

--- src/main/java/controller/MostraPaginaLogin.java
+++ src/main/java/controller/MostraPaginaLogin.java
@@ -1,4 +1,4 @@
-package autenticazione.controller;
+package controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -7,25 +7,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import registrazione.Utente;
+import model.*;
 
 import java.io.IOException;
 
-/**
- * La classe permette l'indirizzamento verso la pagina di compilazione
- * form della login.
- * @author Francesco Di Domenico
- */
+
 
 @WebServlet("/MostraPaginaLogin")
 public class MostraPaginaLogin extends HttpServlet {
 
-    /**
-     * Il metodo permette di gestire la richiesta del client,dove il server
-     * rimanda alla pagina di login  tramite parametro RequestDispatcher .
-     * @param req : oggetto di richiesta HTTP
-     * @param resp : oggetto di risposta HTTP
-     */
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
